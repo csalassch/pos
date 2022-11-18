b4c77a7e712110c3eb640e8e9abd8bbdfe79feac
RicardoLOOR <116845077+RicardoLOOR@users.noreply.github.com>
2022-11-18 13:09:39 -0600

    Mod home

--- src/views/dashboards/Home.js
+++ src/views/dashboards/Home.js
@@ -1,12 +1,92 @@
+import Chart from 'react-apexcharts';
 import BreadCrumbs from '../../layouts/breadcrumbs/BreadCrumbs';
-import ProjectMonth from '../../components/dashboard/analytical/ProjectMonth';
+
+import ComponentCard from '../../components/ComponentCard';
 
 const Home = () => {
-  
+  const seriescolumn = [
+    {
+      name: 'Desktop',
+      data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
+    },
+    {
+      name: 'Mobile',
+      data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
+    },
+    {
+      name: 'Other',
+      data: [35, 41, 36, 26, 45, 48, 52, 53, 41],
+    },
+  ];
+  const optionscolumn = {
+    colors: ['#745af2', '#263238', '#4fc3f7'],
+    chart: {
+      fontFamily: "'Rubik', sans-serif",
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        endingShape: 'rounded',
+        columnWidth: '55%',
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      show: true,
+      width: 2,
+      colors: ['transparent'],
+    },
+    xaxis: {
+      categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+      labels: {
+        style: {
+          cssClass: 'grey--text lighten-2--text fill-color',
+        },
+      },
+    },
+    yaxis: {
+      title: {
+        text: '$ (thousands)',
+        color: '#8898aa',
+      },
+      labels: {
+        style: {
+          cssClass: 'grey--text lighten-2--text fill-color',
+        },
+      },
+    },
+    fill: {
+      opacity: 1,
+    },
+    tooltip: {
+      theme: 'dark',
+      y: {
+        formatter(val) {
+          return `$ ${val} thousands`;
+        },
+      },
+    },
+    grid: {
+      borderColor: 'rgba(0,0,0,0.1)',
+    },
+    legend: {
+      show: true,
+      position: 'bottom',
+      width: '50px',
+      fontFamily: "'Montserrat', sans-serif",
+      labels: {
+        colors: '#8898aa',
+      },
+    },
+  };
   return (
     <>
       <BreadCrumbs />
-      <ProjectMonth/>
+      <ComponentCard title="Column Chart">
+        <Chart options={optionscolumn} series={seriescolumn} type="bar" height="280" />
+      </ComponentCard>
     </>
   );
 };
