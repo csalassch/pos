06a81ef5bef02d37bf952b55273d2acd3ad9ed6a
MagdielJT <magdiel.jimenez@freebug.mx>
2022-11-18 13:24:17 -0600

    Ajuste logos

--- src/layouts/logo/Logo.js
+++ src/layouts/logo/Logo.js
@@ -1,15 +1,15 @@
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import React from 'react';
 // import { ReactComponent as LogoDarkIcon } from '../../assets/images/logos/Logo-FreeBug-web-2-500px.svg';
 // import { ReactComponent as LogoDarkText } from '../../assets/images/logos/dark-logo-text.svg';
 import { ReactComponent as LogoDarkText } from '../../assets/images/logos/Logo-FreeBug-web-2-500px.svg';
-// import { ReactComponent as LogoWhiteIcon } from '../../assets/images/logos/Logo-FreeBug-web-2-500px.svg';
+import { ReactComponent as LogoWhiteIcon } from '../../assets/images/logos/Logo-FreeBug-sello2-_1_.svg';
 // import { ReactComponent as LogoWhiteText } from '../../assets/images/logos/Logo-FreeBug-web-2-500px.svg';
 
 const Logo = () => {
   // const isDarkMode = useSelector((state) => state.customizer.isDark);
-  // const toggleMiniSidebar = useSelector((state) => state.customizer.isMiniSidebar);
+  const toggleMiniSidebar = useSelector((state) => state.customizer.isMiniSidebar);
   // const activeTopbarBg = useSelector((state) => state.customizer.topbarBg);
   return (
     <Link to="/" className="d-flex align-items-center gap-2">
@@ -18,12 +18,12 @@ const Logo = () => {
           <LogoDarkText />
           {toggleMiniSidebar ? '' : <LogoDarkText />}
         </>
-      ) : (
-        <> */}
-          <LogoDarkText style={{height:"50px",width:"auto"}} />
-          {/* {toggleMiniSidebar ? '' : <LogoDarkText />}
-        </>
-      )} */}
+      ) : ( */}
+      <>
+        {!toggleMiniSidebar ? <LogoDarkText style={{ height: "50px", width: "auto" }} />
+          : <LogoWhiteIcon style={{ height: "auto", width: "45px" }} />}
+      </>
+      {/* )} */}
     </Link>
   );
 };
